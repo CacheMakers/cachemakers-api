e0af18ae16ed5694285aa695718827ce43a6d5e3
Nick <nick.benoit14@gmail.com>
2016-05-27 07:33:09 -0600

    added basic fields

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ActiveRecord::Base
+  validates :title, length: { minimum: 1, maximum: 100}, presence: true
+  validates :location, inclusion: {in: %w(makeshop computerlab)}, presence: true
+  validates :time, inclusion: {in: %w(morning afternoon evening)}, presence: true
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -9,23 +9,31 @@ class GroupTest < ActiveSupport::TestCase
   end
 
   test "title should validate" do
-    assert @valid_group.valid?
-    assert !@invalid_group.valid?
+    @valid_group.valid?
+    @invalid_group.valid?
+    assert !@valid_group.errors[:title].any?
+    assert @invalid_group.errors[:title].any?
   end
 
   test "description should validate" do
-    assert @valid_group.valid?
-    assert !@invalid_group.valid?
+    @valid_group.valid?
+    @invalid_group.valid?
+    assert !@valid_group.errors[:description].any?
+    assert !@invalid_group.errors[:description].any?
   end
 
   test "location should validate" do
-    assert @valid_group.valid?
-    assert !@invalid_group.valid?
+    @valid_group.valid?
+    @invalid_group.valid?
+    assert !@valid_group.errors[:location].any?
+    assert @invalid_group.errors[:location].any?
   end
 
   test "time should validate" do
-    assert @valid_group.valid?
-    assert !@invalid_group.valid?
+    @valid_group.valid?
+    @invalid_group.valid?
+    assert !@valid_group.errors[:time].any?
+    assert @invalid_group.errors[:time].any?
   end
 
   test "should validate" do
