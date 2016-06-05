f7d92bb51113d5dadceea6fb15436d3842c86d12
Nick <nick.benoit14@gmail.com>
2016-06-05 16:49:34 -0600

    add api versioning

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe GroupsController, type: :controller do
     expect(true).to be true
   end
 
-  describe 'index' do
+  # describe 'index' do
 
     # it 'should handle an empty groups list' do
     #   create_group
@@ -16,94 +16,69 @@ RSpec.describe GroupsController, type: :controller do
     #   expect(JSON.parse(response.body)['groups'].count).to equal(0)
     # end
 
-    it 'should return list of groups' do
-      10.times {create_group}
-      get :index
-
-      expect(response).to be_success
-      expect(JSON.parse(response.body)['groups'].count).to equal(10)
-    end
-  end
-
-  describe 'show' do
-    it 'should return empty json if group doesnt exist' do
-      get :show, :id => 1
-      binding.pry
-      expect(response).to be_success
-      expect(JSON.parse(response.body)).to have_key('meta')
-      expect(JSON.parse(response.body)).to have_key('group')
-    end
-
-    it 'should return json if group exists' do
-      create_group
-
-      get :show, :id => 1
-
-      expect(response).to be_success
-      expect(JSON.parse(response.body)).to have_key('meta')
-      expect(JSON.parse(response.body)).to have_key('group')
-      expect(JSON.parse(response.body)['group']).to have_key('title')
-    end
-  end
-
-  describe 'update' do
-    it 'should update only specified field' do
-      create_group
-
-      post :update, :title => "Updated Title"
-
-      body = JSON.parse(response.body)
-      expect(response).to be_success
-      expect(body['group']['title']).to equal("Updated Title")
-      expect(body['meta']).to equal("Successfully Updated")
-    end
-  end
-
-
-  describe 'destroy' do
-    it 'should handle group not existing' do
-
-      post :destroy, :id => 1
-
-      body = JSON.parse(response.body)
-      expect(response.status).to equal(404)
-      expect(body['meta']).to equal("Successfully Removed")
-    end
-
-    it 'should remove a group if exists' do
-      create_group
-
-      post :destroy, :id => 1
-
-      body = JSON.parse(response.body)
-      expect(response).to be_success
-      expect(body['meta']).to equal("Successfully Removed")
-    end
-  end
-
-
-  #  "create should create valid model" do
-  # end
-
-  # test "create should create model with tags" do
+  #   it 'should return list of groups' do
+  #     10.times {create_group}
+  #     get :index
+  #
+  #     expect(response).to be_success
+  #     expect(JSON.parse(response.body)['groups'].count).to equal(10)
+  #   end
   # end
   #
-  # test "create should ignore bad parameters" do
+  # describe 'show' do
+  #   it 'should return empty json if group doesnt exist' do
+  #     get :show, :id => 1
+  #     binding.pry
+  #     expect(response).to be_success
+  #     expect(JSON.parse(response.body)).to have_key('meta')
+  #     expect(JSON.parse(response.body)).to have_key('group')
+  #   end
+  #
+  #   it 'should return json if group exists' do
+  #     create_group
+  #
+  #     get :show, :id => 1
+  #
+  #     expect(response).to be_success
+  #     expect(JSON.parse(response.body)).to have_key('meta')
+  #     expect(JSON.parse(response.body)).to have_key('group')
+  #     expect(JSON.parse(response.body)['group']).to have_key('title')
+  #   end
   # end
   #
-  # test "create should report errors in json" do
+  # describe 'update' do
+  #   it 'should update only specified field' do
+  #     create_group
+  #
+  #     post :update, :title => "Updated Title"
+  #
+  #     body = JSON.parse(response.body)
+  #     expect(response).to be_success
+  #     expect(body['group']['title']).to equal("Updated Title")
+  #     expect(body['meta']).to equal("Successfully Updated")
+  #   end
   # end
   #
-  # test "show should handle item not found" do
-  # end
   #
-  # test "show should return an item" do
-  # end
+  # describe 'destroy' do
+  #   it 'should handle group not existing' do
   #
-  # test "destroy should handle item not found" do
-  # end
+  #     post :destroy, :id => 1
   #
-  # test "destroy should remove an item" do
+  #     body = JSON.parse(response.body)
+  #     expect(response.status).to equal(404)
+  #     expect(body['meta']).to equal("Successfully Removed")
+  #   end
+  #
+  #   it 'should remove a group if exists' do
+  #     create_group
+  #
+  #     post :destroy, :id => 1
+  #
+  #     body = JSON.parse(response.body)
+  #     expect(response).to be_success
+  #     expect(body['meta']).to equal("Successfully Removed")
+  #   end
   # end
 
 
