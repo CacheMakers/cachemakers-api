f943338940c37caaf5c155b53624e4b8ff126fab
Nick <nick.benoit14@gmail.com>
2016-06-06 21:10:00 -0600

    added auth token to user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,13 +7,11 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-
     if user.save
       render json: user, status:201
     else
       render json: {errors: user.errors}, status:422
     end
-
   end
 
   def update
