6d14df3fbc75be713544ae304d8aec0ce9655cb6
Nick <nick.benoit14@gmail.com>
2016-06-07 06:26:45 -0600

    added unique token to model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:each){@User = FactoryGirl.create :user}
+  before(:each){@user = FactoryGirl.create :user}
 
   it 'should respond to fields' do
-    expect(@User).to respond_to(:email)
-    expect(@User).to respond_to(:password)
-    expect(@User).to respond_to(:password_confirmation)
-    expect(@User).to respond_to(:auth_token)
+    expect(@user).to respond_to(:email)
+    expect(@user).to respond_to(:password)
+    expect(@user).to respond_to(:password_confirmation)
+    expect(@user).to respond_to(:auth_token)
   end
+
+  describe 'generates unique token' do
+    it 'generates a unique token' do
+      allow(Devise).to receive(:friendly_token){"auniquetoken123"}
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql("auniquetoken123")
+    end
+
+    it 'regenerates a new token if tries existing token' do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql("auniquetoken123")
+    end
+  end
+
 
 end
